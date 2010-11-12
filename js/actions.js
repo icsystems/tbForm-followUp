3ccb863ec1261153bb54e2f72c8f776a92377450
Luiz Fernando de Frias <l.frias@globo.com>
2010-11-12 17:13:15 -0200

    Mudança na apresentação do form de exames

--- js/actions.js
+++ js/actions.js
@@ -735,8 +735,8 @@ $(document).ready(function(){
 	});
 /*---------------------------------------------------------------------------*/
 /*------------------------- Load Exames Form --------------------------------*/
-	/*var url=$(location).attr('href');
-	var numForm = url[url.length - 4];
+	var url=$(location).attr('href');
+	var numForm = parseInt(url[url.length - 4],10) - 1;
 	var numPatient = url[url.length - 2];
 	var sUrl='../../../patientLastRegister/' + numForm + '/' + numPatient + '/';
 	var edits = new Object();
@@ -763,7 +763,7 @@ $(document).ready(function(){
 				$('#divExames').html("A busca não encontrou resultados");
 			}
 		}
-	});*/
+	});
 /*---------------------------------------------------------------------------*/
 /*-------------------------- Form Validation --------------------------------*/
 	$('#form_followup').validate();
